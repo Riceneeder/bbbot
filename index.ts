0250bd8b5eeb8c206614eaaa87a84f54bd5752ac
梵高先生 <845541909@qq.com>
2022-05-27 22:12:15 +0800

    更新插件

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import LuckDraw from "./src/plugins/LuckDraw";
 // import webShotCut from "./src/plugins/webShotCut";  此插件需配置token，详细请进入插件文件src/plugins/webShotCut.ts
 import welcome from "./src/plugins/welcome";
 import song from "./src/plugins/song";
+import searchForQuestions from "./src/plugins/searchForQuestions";
 //========start
 const App = new app(ConfigList.Qid);
 App.boot();
@@ -20,3 +21,4 @@ App.use(ICP);
 App.use(LuckDraw);
 App.use(welcome);
 App.use(song);
+App.use(searchForQuestions);
---
+++ src/plugins/searchForQuestions.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Client } from "oicq";
+//==========配置区===============
+const uid = 13386;
+const token = '0BCmOn6WzG2djAUSJPDw2pNjqx5VNubX';
+//==============================
+const api = 'https://api.wanshiwu.asia/api/search';
+export default function searchForQuestions(bot: Client) {
+    bot.on('message', event => {
+        if (event.raw_message.indexOf('搜题') == 0 && event.raw_message != '搜题' && event.sender.user_id == 845541909) {
+            let question = event.raw_message.replace('搜题', '').trim();
+            axios.get(api, {
+                params: {
+                    uid: uid,
+                    token: token,
+                    question: question
+                }
+            }).then(
+                re => {
+                    let result = `答案：${re.data.data[0].details.answer}\n可信度：${re.data.data[0].score}`;
+                    event.reply(result, true);
+                }
+            ).catch(
+                err => {
+                    event.reply('failed,something goes wrong', true);
+                }
+            )
+        }
+    })
+}
